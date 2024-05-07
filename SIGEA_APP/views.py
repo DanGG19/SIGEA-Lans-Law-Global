96540577cd9ab14c7798013a02c314b67de28b07
DanGG19 <gg19083@ues.edu.sv>
2024-05-06 22:31:20 -0600

    Optimización de proyecto
    Eliminación de archivos innecesarios del proyecto, reducción de código en main.html y creación de templatetag de FOOTER
    
    Co-Authored-By: Allan Augusto Anduray Portillo <90524934+Anduray555@users.noreply.github.com>
    Co-Authored-By: EstherCarcamo <112916820+EstherCarcamo@users.noreply.github.com>
    Co-Authored-By: abner-rivas <90056787+abner-rivas@users.noreply.github.com>
    Co-Authored-By: RodGonzal76 <71513914+RodGonzal76@users.noreply.github.com>

--- SIGEA_APP/views.py
+++ SIGEA_APP/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'SIGEA_APP/index.html')
+    return render(request, 'SIGEA_APP/index.html' )
 
 def login(request):
     return render(request, 'SIGEA_APP/login.html')
